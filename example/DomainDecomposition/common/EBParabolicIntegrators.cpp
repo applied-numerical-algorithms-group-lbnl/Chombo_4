5ecdd81d91be40ff6b036a39934a6ce817e4b899
dtgraves <dtgraves@lbl.gov>
2020-01-15 16:07:52 -0800

    got parabolic driver for backward euler written

--- example/DomainDecomposition/common/EBParabolicIntegrators.cpp
+++ example/DomainDecomposition/common/EBParabolicIntegrators.cpp
@@ -6,6 +6,7 @@
 
 typedef Proto::Var<Real, 1> Sca;
 
+///
 PROTO_KERNEL_START 
 void  getConsDiffF(Sca     a_diff,
                    Sca     a_phiold,
@@ -14,6 +15,19 @@ void  getConsDiffF(Sca     a_diff,
   a_diff(0) = (a_diff(0) - a_phiold(0))/a_dt;
 }
 PROTO_KERNEL_END(getConsDiffF, getConsDiff)
+///
+PROTO_KERNEL_START 
+void  setEulerRHSF(Sca     a_rhs,
+                   Sca     a_phiold,
+                   Sca     a_source,
+                   Sca     a_kappa,
+                   Real    a_dt)
+{
+  //source is presumed to already be kappa weighted
+  a_rhs(0) = a_kappa(0)*a_phiold(0) + a_dt*a_source(0);
+}
+PROTO_KERNEL_END(setEulerRHSF, setEulerRHS)
+///
 
 ///this is the conservative way to advance a solution with diffusion.
 void 
@@ -21,10 +35,14 @@ BaseEBParabolic::
 computeDiffusion( EBLevelBoxData<CELL, 1>       &  a_diffusionTerm,
                   const EBLevelBoxData<CELL, 1> &  a_phiold,
                   const Real                    &  a_diffCoef,
-                  const Real                    &  a_dt)
+                  const Real                    &  a_dt, 
+                  const Real                    &  a_tolerance,
+                  const unsigned int            &  a_maxIterations)
+
 {
   //this puts phinew into diffterm
-  advanceOneStep(a_diffusionTerm, a_phiold, a_diffCoef, a_dt);
+  advanceOneStep(a_diffusionTerm, a_phiold, a_diffCoef, 
+                 a_dt, a_tolerance, a_maxIterations);
   //this sets diffusion term= (phinew-phiold)/a_dt
   DataIterator dit = m_grids.dataIterator();
   for(unsigned int ibox = 0; ibox < dit.size(); ++ibox)
@@ -37,6 +55,41 @@ computeDiffusion( EBLevelBoxData<CELL, 1>       &  a_diffusionTerm,
 
     ebforallInPlace(numflopspt, "getConsDiff", getConsDiff, grid, difffab, phiofab, a_dt);
   }
+}
+///
+void 
+EBBackwardEuler::
+advanceOneStep( EBLevelBoxData<CELL, 1>       &  a_phi,
+                const EBLevelBoxData<CELL, 1> &  a_source,
+                const Real                    &  a_diffCoef,
+                const Real                    &  a_dt,
+                const Real                    &  a_tolerance,
+                const unsigned int            &  a_maxIterations)
+{
+  EBLevelBoxData<CELL, 1> & kappa = m_diffusionSolver->getKappa();
+
+  //set rhs = kappa*phi^n + dt*source 
+  //the source is assumed to already be kappa weighted
+  DataIterator dit = m_grids.dataIterator();
+  for(unsigned int ibox = 0; ibox < dit.size(); ++ibox)
+  {
+    auto       & rhsfab =    m_rhs[dit[ibox]];
+    auto       & phifab =    a_phi[dit[ibox]];
+    const auto & soufab = a_source[dit[ibox]];
+    const auto & kapfab =    kappa[dit[ibox]];
+
+    Bx grid = ProtoCh::getProtoBox(m_grids[dit[ibox]]);
+
+    unsigned long long int numflopspt = 3;
+
+    ebforallInPlace(numflopspt, "setEulerRHS", setEulerRHS, grid, rhsfab, phifab, soufab, kapfab, a_dt);
+  }
+  
+  //solve for new phi
+  Real alpha = 1;
+  Real beta = -a_dt*a_diffCoef;
+  m_diffusionSolver->resetAlphaAndBeta(alpha, beta);
+  m_diffusionSolver->solve(a_phi, m_rhs, a_tolerance, a_maxIterations);
 }
 
 #include "Chombo_NamespaceFooter.H"
