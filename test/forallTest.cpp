ccd7d9e091f81cd0b3e23161d650f5b3e7c560bb
Dan Graves <dtgraves@lbl.gov>
2020-08-12 19:37:32 -0400

    fiddling with tests

--- test/forallTest.cpp
+++ test/forallTest.cpp
@@ -41,6 +41,25 @@ unsigned int  addAlphaPhiF(Var<Real, 1>     a_lph,
   return 0;
 }
 PROTO_KERNEL_END(addAlphaPhiF, addAlphaPhi)
+PROTO_KERNEL_START 
+unsigned int  addAlphaPhiPtF(int a_pt[DIM],
+                             Var<Real, 1>     a_lph,
+                             Var<Real, 1>     a_phi,
+                             Var<Real, 1>     a_kap,
+                             Real    a_alpha,
+                             Real    a_beta)
+{
+  //kappa and beta are already in lph
+  //kappa because we did not divide by kappa
+  //beta was sent in to ebstencil::apply
+  Real kappdiv = a_lph(0);
+  Real bkdivF  = a_beta*kappdiv;
+  Real phival  = a_phi(0);
+  Real kapval  = a_kap(0);
+  a_lph(0) = a_alpha*phival*kapval + bkdivF;
+  return 0;
+}
+PROTO_KERNEL_END(addAlphaPhiPtF, addAlphaPhiPt)
 /****/
 //lph comes in holding beta*div(F)--leaves holding alpha phi + beta div(F)
 PROTO_KERNEL_START 
@@ -109,8 +128,10 @@ runTest(int a_argc, char* a_argv[])
     EBBoxData<CELL, Real, 1>& lphbd = lph[dit[ibox]];
     EBBoxData<CELL, Real, 1>& kapbd = kap[dit[ibox]];
     Bx grbx = phibd.box();
-    size_t numflopspt = 0;
-    ebforallInPlace_i(numflopspt, "setPhiPt", setPhiPt, grbx, phibd, dx);
+    Bx inputBox = phibd.box();
+//    size_t numflopspt = 0;
+//    ebforallInPlace_i(numflopspt, "setPhiPt", setPhiPt, grbx, phibd, dx);
+    ebforall_i(inputBox, setPhiPt, grbx, phibd, dx);
     lphbd.setVal(1.0);
     kapbd.setVal(1.0);
   }
