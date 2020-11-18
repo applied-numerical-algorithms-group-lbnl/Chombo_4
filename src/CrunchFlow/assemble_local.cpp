47544ec0749a03332fea4d18135737dcd8817efa
cpaolini <paolini@engineering.sdsu.edu>
2020-11-18 13:44:49 -0800

    Progress on assemble_local kernel

--- src/CrunchFlow/assemble_local.cpp
+++ src/CrunchFlow/assemble_local.cpp
@@ -19,11 +19,89 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef PROTO_CUDA
+#  include <assert.h>
+#endif
+
 #include "ShapeArray.H"
 #include "crunchflow.h"
 
+#define get2D(A,i,j,Ni,Nj) (A[(i)*(Nj) + (j)])
+#define get3D(A,i,j,k,Ni,Nj,Nk) (A[(i) * (Nj) * (Nk) + (j) * (Nk) + (k)])
+
 using namespace shape;
 
+#ifdef PROTO_CUDA
+
+__global__ void assemble_local_kernel(double* dA,
+			 double* dSatliq,
+			 const int neqn,
+			 const int ncomp,
+			 const int nkin,
+			 const int nx,
+			 const int ny,
+			 const int nz,
+			 const int nradmax,
+			 const double dt,
+			 int *nreactmin, // [nkin]
+			 double *rmin, // [nkin][MAX_PATH]
+			 double *decay_correct, // [nkin][ncomp]
+			 double *mumin) // [ncomp][nkin][MAX_PATH]
+{
+  double r = 1.0/dt;
+  double sumrct;
+  double sumkin;
+  int ind;
+
+  for (int i = 1; i <= ncomp; ++i)
+    {
+      ind = i;
+
+    if (nradmax > 0)
+      {
+	sumrct = 0.0;
+	for (int k = 1; k <= nkin; ++k)
+	  {
+	    for (int npIndex = 1; npIndex <= nreactmin[k-1]; ++npIndex)
+	      {
+		if (get2D(rmin,k-1,npIndex-1,nkin,MAX_PATH) /* rmin[k-1][npIndex-1] */ >= 0.0)
+		  {
+		    // sumrct = sumrct + decay_correct[k-1][i-1] * mumin[i-1][k-1][npIndex-1] * rmin[k-1][npIndex-1];
+		    sumrct = sumrct +
+		      get2D(decay_correct,k-1,i-1,nkin,ncomp) *
+		      get3D(mumin,i-1,k-1,npIndex-1,ncomp,nkin,MAX_PATH) *
+		      get2D(rmin,k-1,npIndex-1,nkin,MAX_PATH);
+		  }
+		else
+		  {
+		    //fprintf(stderr, "assemble_local_cpp.cpp: mumin_decay not implemented in crunchflow lite\n");
+		    //exit(1);
+		    return;
+		  }
+	      }
+	  }
+      }
+    else
+      {
+	sumrct = 0.0;
+	for (int k = 1; k <= nkin; ++k)
+	  {
+	    for (int npIndex = 1; npIndex <= nreactmin[k-1]; ++npIndex)
+	      {
+		// sumrct = sumrct + decay_correct[k-1][i-1] * mumin[i-1][k-1][npIndex-1] * rmin[k-1][npIndex-1];
+		sumrct = sumrct +
+		  get2D(decay_correct,k-1,i-1,nkin,ncomp) *
+		  get3D(mumin,i-1,k-1,npIndex-1,ncomp,nkin,MAX_PATH) *
+		  get2D(rmin,k-1,npIndex-1,nkin,MAX_PATH);
+	      }
+	  }
+      }
+    sumkin = 0.0;
+    }
+}
+#endif
+  
 void assemble_local(const int ncomp, 
 		    const int nspec, 
 		    const int nkin, 
@@ -81,10 +159,68 @@ void assemble_local(const int ncomp,
   double sumrd[neqn]; // allocate(sumrd(neqn),stat=ierr); sumrd = 0.0d0
   double sumjackin[neqn]; // allocate(sumjackin(neqn),stat=ierr); sumjackin = 0.0d0
 
+#ifdef PROTO_CUDA
+
+  cudaError_t cudaStat = cudaSuccess;
+
+  size_t pitch;
+  double *dA;
+  
+  cudaStat = cudaMalloc ((void**)&dA, sizeof(double) * neqn * neqn );
+  assert(cudaSuccess == cudaStat);
+
+  double *dSatliq;
+  cudaStat = cudaMalloc ((void**)&dSatliq, sizeof(double) * nx * ny * nz );
+  assert(cudaSuccess == cudaStat);
+
+  int *dnreactmin;
+  cudaStat = cudaMalloc ((void**)&dnreactmin, sizeof(int) * nkin );
+  assert(cudaSuccess == cudaStat);
+
+  cudaStat = cudaMemcpy (dnreactmin, nreactmin_1d, sizeof(int) * nkin, cudaMemcpyHostToDevice );
+  assert(cudaSuccess == cudaStat);
+
+  double *drmin;
+  cudaStat = cudaMalloc ((void**)&drmin, sizeof(double) * nkin * MAX_PATH );
+  assert(cudaSuccess == cudaStat);
+  cudaStat = cudaMemcpy (drmin, rmin_2d, sizeof(double) * nkin * MAX_PATH, cudaMemcpyHostToDevice );
+  assert(cudaSuccess == cudaStat);
+
+  double *ddecay_correct;
+  cudaStat = cudaMalloc ((void**)&ddecay_correct, sizeof(double) * nkin * ncomp );
+  assert(cudaSuccess == cudaStat);
+  cudaStat = cudaMemcpy (ddecay_correct, decay_correct_2d, sizeof(double) * nkin * ncomp, cudaMemcpyHostToDevice );
+  assert(cudaSuccess == cudaStat);
+
+  double *dmumin;
+  cudaStat = cudaMalloc ((void**)&dmumin, sizeof(double) * ncomp * nkin * MAX_PATH );
+  assert(cudaSuccess == cudaStat);
+  cudaStat = cudaMemcpy (dmumin, mumin_3d, sizeof(double) * ncomp * nkin * MAX_PATH, cudaMemcpyHostToDevice );
+  assert(cudaSuccess == cudaStat);
+  
+  assemble_local_kernel<<<1, 1>>>(dA,
+		     dSatliq,
+		     neqn,
+		     ncomp,
+		     nkin,
+		     nx,
+		     ny,
+		     nz,
+		     nradmax,
+		     dt,
+		     dnreactmin,
+		     drmin,
+		     ddecay_correct,
+		     dmumin);
+
+#else
+  
   for(size_t i = 0; i < ncomp; ++i)
     for(size_t j = 0; j < ncomp; ++j)
       aaa[i][j] = 0.0;
 
+#endif
+  
   source = 0.0;
   retardation = 1.0;
 
@@ -92,14 +228,24 @@ void assemble_local(const int ncomp,
   portemp = por[jz-1][jy-1][jx-1];
   rotemp  = ro[jz-1][jy-1][jx-1];
   xgtemp = xgram[jz-1][jy-1][jx-1];
+
+#ifdef PROTO_CUDA
+
+  size_t pitchSatliq;
+  size_t pitchPor;
+  size_t pitchRo;
+  size_t pitchXGram;
+
+  double *dPor;
+  double *dRo;
+  double *dXGram;
+  
+#endif
+
   r = 1.0/dt;
 
   for (int i = 1; i <= ncomp; ++i) {
     ind = i;
-
-    int bkpt = 0;
-    if(jz == 1 && jy == 1 && jx == 1 && ind == 1)
-      bkpt = 1;
 
     if (nradmax > 0) {
       sumrct = 0.0;
