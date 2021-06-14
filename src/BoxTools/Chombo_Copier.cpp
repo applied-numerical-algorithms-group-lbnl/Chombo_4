9d824bfc96a596dc8b5eb73a5a49ce324a327ee4
rpratfr <rprat@lbl.bov>
2021-06-14 09:29:36 -0700

    move allocation copierbuffer allocation to copierbuffer class + add a condition (!=nullptr) to release memory

--- src/BoxTools/Chombo_Copier.cpp
+++ src/BoxTools/Chombo_Copier.cpp
@@ -36,7 +36,7 @@ CopierBuffer::~CopierBuffer()
 void CopierBuffer::clear()
 {
 #ifdef PROTO_CUDA
-  if(m_sendbuffer != NULL)
+  if(m_sendbuffer != nullptr)
   {
     protoPointerAttributes att;
     protoPointerGetAttributes(&att, m_sendbuffer);
@@ -48,7 +48,7 @@ void CopierBuffer::clear()
     else {protoFreeHost(m_sendbuffer);} 
   }
   
-  if (m_recbuffer  != NULL) 
+  if (m_recbuffer  != nullptr) 
   {
     protoPointerAttributes att;
     protoPointerGetAttributes(&att, m_recbuffer);
@@ -60,14 +60,54 @@ void CopierBuffer::clear()
     else { protoFreeHost(m_recbuffer);}	  
   }
 #else
-  if (m_sendbuffer != NULL) free(m_sendbuffer);
-  if (m_recbuffer  != NULL) free(m_recbuffer);
+  if (m_sendbuffer != nullptr) free(m_sendbuffer);
+  if (m_recbuffer  != nullptr) free(m_recbuffer);
 #endif
-  m_sendbuffer = NULL;
-  m_recbuffer  = NULL;
+  m_sendbuffer = nullptr;
+  m_recbuffer  = nullptr;
   m_sendcapacity = 0;
   m_reccapacity = 0;
   m_ncomps = 0;
+}
+
+// template T is used to know what kind of memory is used. Maybe we could extend the memtype here.
+template<typename T>
+void CopierBuffer::reallocate(unsigned int a_sendBufferSize, unsigned int a_recBufferSize, unsigned int s_verbosity)
+{
+  auto Mem = T::memTypeAllocation();
+  auto fun = [Mem] (void* a_ptr, unsigned int a_size)
+  {
+    if(Mem)
+#ifdef PROTO_CUDA
+    {
+      if(a_ptr != nullptr) {protoFree(MEMTYPE_DEFAULT,a_ptr);}
+      protoMalloc(MEMTYPE_DEFAULT,a_ptr,a_size);
+    }
+    else
+    {
+      if(a_ptr != nullptr) protoFreeHost(a_ptr);
+      protoHostAlloc(a_ptr,a_size);
+    }
+#else
+    if(a_ptr != nullptr) free((m_buff->m_sendbuffer));
+    a_ptr = malloc(a_size);
+#endif
+  };
+
+  if(a_sendBufferSize > m_sendcapacity) 
+  {
+    fun(m_sendbuffer, a_sendBufferSize);
+    if (s_verbosity > 0) pout()<<"malloc send buffer "<<a_sendBufferSize<<std::endl;
+    m_sendcapacity = a_sendBufferSize;
+  }
+
+  if(a_recBufferSize > m_reccapacity) 
+  {
+    fun(m_recbuffer, a_recBufferSize);
+    if (s_verbosity > 0) pout()<<"malloc send buffer "<<a_recBufferSize<<std::endl;
+    m_reccapacity = a_recBufferSize;
+    assert(m_recbuffer!=nullptr);
+  }
 }
 
 Copier::Copier(const DisjointBoxLayout& a_level,
