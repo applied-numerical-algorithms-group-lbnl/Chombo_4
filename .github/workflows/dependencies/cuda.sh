d4b9bec9d9d3eff86b2bd2bc2ea930851c71927a
dwatersg <dwaters@lbl.gov>
2021-11-19 01:14:20 -0500

    need blas and lapack packages

--- .github/workflows/dependencies/cuda.sh
+++ .github/workflows/dependencies/cuda.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 sudo apt-get -qq update
-sudo apt-get install -y cmake g++ wget
+sudo apt-get install -y cmake g++ wget libblas-dev liblapack-dev
 
 sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo apt-key add 7fa2af80.pub
