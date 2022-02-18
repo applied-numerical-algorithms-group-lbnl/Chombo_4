4dc9c3596cb2a3aea62a7c80125980940ef08143
dtgraves <dtgraves@lbl.gov>
2022-02-18 15:43:45 -0800

    this compiles with the ebchombo branch of Proto

--- examples/DomainDecomposition/EBHelmholtz/exec/HelmholtzSolve.cpp
+++ examples/DomainDecomposition/EBHelmholtz/exec/HelmholtzSolve.cpp
@@ -24,6 +24,7 @@
 int
 runTest(int a_argc, char* a_argv[])
 {
+  using Chombo4::pout;
   Real coveredval = -1;
   int nx      = 32;
   int maxGrid = 32;
@@ -230,7 +231,7 @@ int main(int a_argc, char* a_argv[])
     ParmParse  pp(a_argc-2,a_argv+2,NULL,in_file);
     runTest(a_argc, a_argv);
   }
-
+  using Chombo4::pout;
   pout() << "printing time table " << endl;
   CH_TIMER_REPORT();
 #ifdef CH_MPI
