7fb60f258a0491cef61ae68fca92374c8f0afdaf
dtgraves <dtgraves@lbl.gov>
2023-08-24 12:19:45 -0700

    seeing if we show lapack the door in this branch

--- src/LinearAlgebra/Chombo_LAPACKMatrix.H
+++
@@ -1,8 +0,0 @@
-/*
- *      _______              __
- *     / ___/ /  ___  __ _  / /  ___
- *    / /__/ _ \/ _ \/  V \/ _ \/ _ \
- *    \___/_//_/\___/_/_/_/_.__/\___/
- *    Please refer to Copyright.txt, in Chombo's root directory.
- */
-
